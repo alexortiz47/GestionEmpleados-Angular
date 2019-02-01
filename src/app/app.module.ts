7489cdbd6cd1a39f328744d25b460c66745db7f0
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-01 19:09:02 +0100

    Añadido cambio de icono en funcion del departamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
 import { ListaEmpleadosComponent } from './lista-empleados/lista-empleados.component';
 import { FormularioComponent } from './formulario/formulario.component';
--- src/app/detalle-empleado/detalle-empleado.component.ts
+++ src/app/detalle-empleado/detalle-empleado.component.ts
@@ -7,12 +7,31 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DetalleEmpleadoComponent implements OnInit {
 
-  @Input() empleado: any[];
+  @Input() empleado: any;
+
+  diseno: boolean;
+  informatica: boolean;
+  marketing: boolean;
+  ventas: boolean;
 
   constructor() {
-    this.empleado = [];
+    this.empleado = {};
+    this.diseno = false;
+    this.informatica = false;
+    this.marketing = false;
+    this.ventas = false;
+
   }
   ngOnInit() {
+    if(this.empleado.depar == 'Diseño') {
+      this.diseno = true;
+    } else if(this.empleado.depar == 'Informática') {
+      this.informatica = true;
+    } else if(this.empleado.depar == 'Marketing') {
+      this.marketing = true;
+    } else if(this.empleado.depar == 'Ventas') {
+      this.ventas = true;
+    }
   }
 
 }
